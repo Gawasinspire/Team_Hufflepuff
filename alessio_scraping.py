44282c773e40b304664c0c11c43cefbe2882b582
alessiorecchia <recchia.alessio@yahoo.com>
2021-04-12 12:52:02 +0200

    created the function to get the urls of the books

--- alessio_scraping.py
+++ alessio_scraping.py
@@ -18,16 +18,3 @@ def get_books_url(base_url, tag, tag_class):
         book_url_list.append('https://www.goodreads.com/' + a['href'])
     
     return book_url_list
-
-# books = get_books_url(base_url, 'a', 'bookTitle')
-# print(books)
-
-book_base_url = 'https://www.goodreads.com/book/show/13496.A_Game_of_Thrones'
-
-book_base_page = rq.get(book_base_url)
-
-book_base_content = BeautifulSoup(book_base_page.content, 'html.parser')
-
-title = book_base_content.find_all('h1', id = 'bookTitle')[0].text
-print(title)
-print(type(title))
