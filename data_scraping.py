e629f33461d24552f286083d2929b58d6e0bebe7
alessiorecchia <recchia.alessio@yahoo.com>
2021-04-13 10:24:16 +0200

    scraping data with BeautifulSoup

--- data_scraping.py
+++ data_scraping.py
@@ -32,53 +32,61 @@ for link in url_list[:20]:
     page_content = BeautifulSoup(page.content, 'html.parser')
 
     # get the genres
-    genre_per_book = []
-    for i in range(3):
-        genre = page_content.find_all('a', class_ = 'actionLinkLite bookPageGenreLink')[i].text
-        genre_per_book.append(genre)
-    genre_list.append(genre_per_book)
+    # genre_per_book = []
+    # for i in range(3):
+    #     genre = page_content.find_all('a', class_ = 'actionLinkLite bookPageGenreLink')[i].text
+    #     genre_per_book.append(genre)
+    # genre_list.append(genre_per_book)
 
     # get awards (if any)
-    if page_content.find_all('div', itemprop = 'awards'):
-        awards = page_content.find_all('div', itemprop = 'awards')[0].text
-        awards_list.append(awards)
-    else:
-        awards_list.append(np.nan)
+    # if page_content.find_all('div', itemprop = 'awards'):
+    #     awards = page_content.find_all('div', itemprop = 'awards')[0].text
+    #     awards_list.append(awards)
+    # else:
+    #     awards_list.append(np.nan)
     
     # get title
-    title = page_content.find_all('h1', itemprop = 'name')[0].text
-    title = title.replace('\n','').strip()
-    title_list.append(title)
+    # title = page_content.find_all('h1', itemprop = 'name')[0].text
+    # title = title.replace('\n','').strip()
+    # title_list.append(title)
 
     # get author name
-    author = page_content.find_all('div', class_ = 'authorName__container')[0].find_all('span', itemprop = 'name')[0].text
-    author_list.append(author)
+    # author = page_content.find_all('div', class_ = 'authorName__container')[0].find_all('span', itemprop = 'name')[0].text
+    # author_list.append(author)
 
     # get num_reviews
-    num_reviews = page_content.find_all('meta', itemprop = 'reviewCount')[0].attrs['content']
-    num_reviews_list.append(num_reviews if page_content.find_all('meta', itemprop = 'reviewCount')[0].attrs['content'] else np.nan)
+    # num_reviews = page_content.find_all('meta', itemprop = 'reviewCount')[0].attrs['content']
+    # num_reviews_list.append(num_reviews if page_content.find_all('meta', itemprop = 'reviewCount')[0].attrs['content'] else np.nan)
 
     # get avg_ratings
-    avg_ratings = page_content.find_all('div', itemprop = 'aggregateRating')[0].find_all('span', itemprop = 'ratingValue')[0].text
-    avg_ratings_list.append(avg_ratings)
+    # avg_ratings = page_content.find_all('div', itemprop = 'aggregateRating')[0].find_all('span', itemprop = 'ratingValue')[0].text
+    # avg_ratings_list.append(avg_ratings)
 
     # get num_pages
-    num_pages = page_content.find_all('span', itemprop="numberOfPages")[0].text
-    num_pages_list.append(num_pages[:-6])
+    # num_pages = page_content.find_all('span', itemprop="numberOfPages")[0].text
+    # num_pages_list.append(num_pages[:-6])
 
     # get original_publish_year
-    original_publish_year = page_content.find_all('nobr', class_ = "greyText")[0].text.strip()
-    original_publish_year_list.append(original_publish_year[-6:-1])
+    # if page_content.find_all('nobr', class_ = "greyText"):
+    #     original_publish_year = page_content.find_all('nobr', class_ = "greyText")[0].text.strip()
+    #     original_publish_year_list.append(original_publish_year[-6:-1])
+    # else:
+    #     original_publish_year = page_content.find_all('div', class_ = "uitext darkGreyText")[0].find_all('div', class_ = 'row')[1].text.split('\n')
+    #     original_publish_year_list.append(original_publish_year[2][-4:])
 
     # get is_series
-    is_series = page_content.find_all('div', id = "bookDataBox")[0].find_all('div', class_ = 'clearFloats')[3].find_all('div', class_ = "infoBoxRowTitle")[0].text
-    is_series_list.append(1 if is_series == 'Series' else 0)
+    if page_content.find_all('div', id = "bookDataBox")[0].find_all('div', class_ = 'clearFloats')[0].find_all('div', class_ = "infoBoxRowTitle")[0].text == 'Original Title':
+        is_series = page_content.find_all('div', id = "bookDataBox")[0].find_all('div', class_ = 'clearFloats')[3].find_all('div', class_ = "infoBoxRowTitle")[0].text
+        is_series_list.append(1 if is_series == 'Series' else 0)
+    else:
+        is_series = page_content.find_all('div', id = "bookDataBox")[0].find_all('div', class_ = 'clearFloats')[3].find_all('div', class_ = "infoBoxRowTitle")[0].text
+        is_series_list.append(1 if is_series == 'Series' else 0)
 
     # get places
 
-df = pd.DataFrame({'title': title_list, 'author': author_list, 'num_reviews': num_reviews_list, 'num_ratings': avg_ratings_list, 'num_pages': num_pages_list, 'original_publish_year': original_publish_year_list})
+# df = pd.DataFrame({'title': title_list, 'author': author_list, 'num_reviews': num_reviews_list, 'num_ratings': avg_ratings_list, 'num_pages': num_pages_list, 'original_publish_year': original_publish_year_list})
 
-print(df)
+# print(df)
     
 
 
